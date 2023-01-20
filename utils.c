7bca7d1f5357e62298c5defefb4099dc97569e47
Dawit Tadesse <114774535+dave0927@users.noreply.github.com>
2023-01-20 22:45:53 +0300

    Update utils.c

--- utils.c
+++ utils.c
@@ -3,14 +3,16 @@
 /**
  * is_printable - Evaluates if a char is printable
  * @c: Char to be evaluated.
- *
  * Return: 1 if c is printable, 0 otherwise
  */
+ 
 int is_printable(char c)
 {
 	if (c >= 32 && c < 127)
+	{
 		return (1);
-
+	}
+	
 	return (0);
 }
 
@@ -21,6 +23,7 @@ int is_printable(char c)
  * @ascii_code: ASSCI CODE.
  * Return: Always 3
  */
+
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
@@ -40,9 +43,9 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 /**
  * is_digit - Verifies if a char is a digit
  * @c: Char to be evaluated
- *
  * Return: 1 if c is a digit, 0 otherwise
  */
+ 
 int is_digit(char c)
 {
 	if (c >= '0' && c <= '9')
@@ -55,9 +58,9 @@ int is_digit(char c)
  * convert_size_number - Casts a number to the specified size
  * @num: Number to be casted.
  * @size: Number indicating the type to be casted.
- *
  * Return: Casted value of num
  */
+
 long int convert_size_number(long int num, int size)
 {
 	if (size == S_LONG)
@@ -72,15 +75,19 @@ long int convert_size_number(long int num, int size)
  * convert_size_unsgnd - Casts a number to the specified size
  * @num: Number to be casted
  * @size: Number indicating the type to be casted
- *
  * Return: Casted value of num
  */
+
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
 	if (size == S_LONG)
+	{
 		return (num);
+	}
 	else if (size == S_SHORT)
+	{
 		return ((unsigned short)num);
-
+	}
+	
 	return ((unsigned int)num);
 }
