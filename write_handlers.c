bf70f2484e5ec610070f6d1294b3ab848a45e5f3
Mercy Girma Adnew <117748372+Sweetpanda68@users.noreply.github.com>
2023-01-20 23:14:12 +0300

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -25,5 +25,20 @@ int handle_write_char(char c, char buffer[], int flags, int width, int precision
 	buffer[i++] = c;
 	buffer[i] = '\0';
 
+	if (width > 1)
+	{
+		buffer[BUFF_SIZE - 1] = '\0';
+		for (i = 0; i < width - 1; i++)
+			buffer[BUFF_SIZE - i - 2] = padd;
+
+		if (flags & F_MINUS)
+			return (write(1, &buffer[0], 1) +
+					write(1, &buffer[BUFF_SIZE - i - 1], width - 1));
+		else
+			return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) +
+					write(1, &buffer[0], 1));
+	}
+
+	return (write(1, &buffer[0], 1));
 }
 
