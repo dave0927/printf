4aafd560afe7b3df2f5ef57d7e55545cd62448ae
Dawit Tadesse <dawittadesse927@gmail.com>
2023-01-20 08:42:26 -0800

    minot fix

--- get_flags.c
+++ get_flags.c
@@ -2,10 +2,11 @@
 
 /**
  * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
+ * @format: Formatted string to print
  * @i: take a parameter.
  * Return: Flags:
  */
+
 int get_flags(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
@@ -25,7 +26,9 @@ int get_flags(const char *format, int *i)
 			}
 
 		if (FLAGS_CH[j] == 0)
+		{
 			break;
+		}
 	}
 
 	*i = curr_i - 1;
