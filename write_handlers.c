b0e0240f21a5b725bfb763d8bedbb6b316ebd1cd
Dawit Tadesse <114774535+dave0927@users.noreply.github.com>
2023-01-20 23:54:43 +0300

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -198,11 +198,10 @@ int write_unsgnd(int is_negative, int ind, char buffer[], int flags, int width, 
  * @padd: Char representing the padding
  * @extra_c: Char representing extra char
  * @padd_start: Index at which padding should start
- *
  * Return: Number of written chars.
  */
-int write_pointer(char buffer[], int ind, int length,
-	int width, int flags, char padd, char extra_c, int padd_start)
+ 
+int write_pointer(char buffer[], int ind, int length, int width, int flags, char padd, char extra_c, int padd_start)
 {
 	int i;
 
@@ -211,35 +210,43 @@ int write_pointer(char buffer[], int ind, int length,
 		for (i = 3; i < width - length + 3; i++)
 			buffer[i] = padd;
 		buffer[i] = '\0';
-		if (flags & F_MINUS && padd == ' ')/* Asign extra char to left of buffer */
+		if (flags & F_MINUS && padd == ' ')
 		{
 			buffer[--ind] = 'x';
 			buffer[--ind] = '0';
+			
 			if (extra_c)
 				buffer[--ind] = extra_c;
+			
 			return (write(1, &buffer[ind], length) + write(1, &buffer[3], i - 3));
 		}
-		else if (!(flags & F_MINUS) && padd == ' ')/* extra char to left of buffer */
+		else if (!(flags & F_MINUS) && padd == ' ')
 		{
 			buffer[--ind] = 'x';
 			buffer[--ind] = '0';
+			
 			if (extra_c)
 				buffer[--ind] = extra_c;
+			
 			return (write(1, &buffer[3], i - 3) + write(1, &buffer[ind], length));
 		}
-		else if (!(flags & F_MINUS) && padd == '0')/* extra char to left of padd */
+		else if (!(flags & F_MINUS) && padd == '0')
 		{
 			if (extra_c)
 				buffer[--padd_start] = extra_c;
+				
 			buffer[1] = '0';
 			buffer[2] = 'x';
-			return (write(1, &buffer[padd_start], i - padd_start) +
-				write(1, &buffer[ind], length - (1 - padd_start) - 2));
+			
+			return (write(1, &buffer[padd_start], i - padd_start) + write(1, &buffer[ind], length - (1 - padd_start) - 2));
 		}
 	}
+	
 	buffer[--ind] = 'x';
 	buffer[--ind] = '0';
+	
 	if (extra_c)
 		buffer[--ind] = extra_c;
+	
 	return (write(1, &buffer[ind], BUFF_SIZE - ind - 1));
 }
