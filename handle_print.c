f7f1568c9379b59a44dacc58101f690492fa5efb
Dawit Tadesse <dawittadesse927@gmail.com>
2023-01-23 08:54:14 -0800

    file type fix

--- handle_print.c
+++ handle_print.c
@@ -13,8 +13,8 @@
  * Return: 1 or 2;
  */
 
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-	int flags, int width, int precision, int size)
+int handle_print(const char *fmt, int *ind, va_list list,
+	char buffer[], int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
@@ -28,24 +28,18 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 	{
 		if (fmt[*ind] == fmt_types[i].fmt)
-		{
 			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
-		}
 	}
 
 	if (fmt_types[i].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
-		{
 			return (-1);
-		}
 
 		unknow_len += write(1, "%%", 1);
 
 		if (fmt[*ind - 1] == ' ')
-		{
 			unknow_len += write(1, " ", 1);
-		}
 
 		else if (width)
 		{
