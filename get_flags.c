a7b5024000bb5dc55e1155ae39155417762c29d6
Dawit Tadesse <114774535+dave0927@users.noreply.github.com>
2023-01-23 19:10:46 +0300

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -4,13 +4,11 @@
 * get_flags - Calculates active flags
 * @format: Formatted string to print
 * @i: take a parameter.
-* Return: Flags:
+* Return: Flags
 */
 
 int get_flags(const char *format, int *i)
 {
-	/* - + 0 # ' ' */
-	/* 1 2 4 8  16 */
 	int j, curr_i;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
@@ -26,9 +24,7 @@ int get_flags(const char *format, int *i)
 			}
 
 		if (FLAGS_CH[j] == 0)
-		{
 			break;
-		}
 	}
 
 	*i = curr_i - 1;
